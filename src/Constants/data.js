a2795f38e1e4076e05afd84c25cbaea9ba5b3654
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-10-31 15:45:51 +0500

    constant-data

---
+++ src/Constants/data.js
@@ -0,0 +1,92 @@
+import images from "./images";
+
+const drinks = [
+  {
+    title: "Pepsi",
+    price: "Rs80",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Coca Cola",
+    price: "Rs80",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Mirinda",
+    price: "Rs80",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Sprite",
+    price: "Rs80",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "7up",
+    price: "Rs80",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Pakola",
+    price: "Rs80",
+    tags: "Pet | Bottle",
+  },
+];
+
+const cocktails = [
+  {
+    title: "Apple",
+    price: "Rs200",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Orange",
+    price: "Rs200",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Watermelon",
+    price: "Rs200",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Pomegranate",
+    price: "Rs200",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Strawberry",
+    price: "Rs200",
+    tags: "Pet | Bottle",
+  },
+  {
+    title: "Chocolate Mash",
+    price: "Rs200",
+    tags: "Pet | Bottle",
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: "Bib Gourmond",
+    subtitle: "No.1 Hygiene Standard Maintenance",
+  },
+  {
+    imgUrl: images.award01,
+    title: "Rising Star",
+    subtitle: "Best Restaurant in Pakistan",
+  },
+  {
+    imgUrl: images.award05,
+    title: "AA Hospitality",
+    subtitle: "Great Hospitality since 1960",
+  },
+  {
+    imgUrl: images.award03,
+    title: "Outstanding Chef",
+    subtitle: "Internationally Highly Qualified Chef",
+  },
+];
+
+export { drinks, cocktails, awards };
