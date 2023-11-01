4ba7d0f0ddd4f3228453d654a6840a9d9edfca18
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-11-01 18:35:25 +0500

    container-index

---
+++ src/container/index.js
@@ -0,0 +1,21 @@
+import AboutUs from "./AboutUs/AboutUs";
+import Footer from "./Footer/Footer";
+import Gallery from "./Gallery/Gallery";
+import Header from "./Header/Header";
+import Intro from "./Intro/Intro";
+import Laurels from "./Laurels/Laurels";
+import SpecialMenu from "./Menu/SpecialMenu";
+import Chef from "./Chef/Chef";
+import FindUs from "./FindUs/FindUs";
+
+export {
+  AboutUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+  Chef,
+  FindUs,
+};
