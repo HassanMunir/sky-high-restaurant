42a530cf72185f488d919488c9dee0291d99b38a
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-11-01 18:33:35 +0500

    app-src

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
 
-const App = () => {
-  return (
-    <div>
-      HighSky Restaurant
-    </div>
-  )
-}
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
 
-export default App
+import { Navbar } from "./components";
+import "./App.css";
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
