febd5bf1df9e551424cece8c6d4f98c448076973
Hassan Munir <91518406+HassanMunir@users.noreply.github.com>
2023-11-02 12:01:29 +0500

    constants-img-upd

--- src/Constants/images.js
+++ src/Constants/images.js
@@ -5,7 +5,7 @@ import gallery01 from "../assets/gallery01.png";
 import gallery02 from "../assets/gallery02.png";
 import gallery03 from "../assets/gallery03.png";
 import gallery04 from "../assets/gallery04.png";
-import knife from '../assets/knife.png';
+import knife from "../assets/knife.png";
 import logo from "../assets/logo.png";
 import menu from "../assets/menu.png";
 import overlaybg from "../assets/overlaybg.png";
@@ -21,7 +21,7 @@ import sign from "../assets/sign.png";
 import quote from "../assets/quote.png";
 import gericht from "../assets/gericht.png";
 
-export {
+export default {
   bg,
   chef,
   G,
